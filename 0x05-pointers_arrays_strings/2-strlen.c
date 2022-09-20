641c80dea1d1733fc38fe3e279d00ac59209d5c9
bamumuney <bamumuney@gmail.com>
2022-09-20 02:37:41 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Write a function that returns the length of a string.
+ * Prototype: int _strlen(char *s);
+ *
+ */
+int _strlen(char *s)
+{
+	int index;
+	for (index =0; s[index] != '\0' ; index++)
+
+	return (index);
+}
