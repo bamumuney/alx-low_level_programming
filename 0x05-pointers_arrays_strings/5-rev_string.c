3eebd576cb6c5b17a53e83659109156f5dd61f5c
bamumuney <bamumuney@gmail.com>
2022-09-20 03:07:26 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * a function that reverses a string
+ * void rev_string(char *s)
+ *
+ */
+void rev_string(char *s)
+{
+	int i, j, k, temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
+	}
+}
